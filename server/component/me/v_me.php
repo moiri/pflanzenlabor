87ea00f7f8d72937e94a8fe4847861f1866925b1
Simon Maurer <moirelein@gmail.com>
2018-03-26 21:05:28 +0200

    add text and image on me

--- server/component/me/v_me.php
+++ server/component/me/v_me.php
@@ -11,8 +11,29 @@
             </div>
         </div>
     </div>
-    <div class="row">
+    <div class="row mt-3">
         <div class="container">
+            <div class="row row-eq-height">
+                <div class="col pr-md-0">
+                    <div class="card h-100">
+                        <div class="card-body">
+Giovina Nicolai ist Dipl. Drogistin HF, Galenikerin und Bierbrauerin.
+Sie ist in einer Berner Apotheke für die Herstellung von Arzneimitteln verantwortlich und ist Prüfungsexpertin für Galenik an der Höheren Fachschule für Drogistinnen und Drogisten.
+In Bern aufgewachsen und wohnhaft, interessierte sie sich schon früh für Pflanzen und die Naturheilkunde und bildete sich über die Jahre in Pflanzenwissen, Pharmatechnologie und Kosmetikherstellung weiter.
+In einem dreijährigen Aufenthalt in London widmete sie sich einer besonderen Pflanze, dem Hopfen, und erlernte den Beruf der Bierbrauerin.
+Sie braute selbst und gab Kurse im Bierbrauen.
+Heute gibt sie ihr umfassendes Wissen über Pflanzen, deren Vorkommen und Wirkung, aber auch deren kulturelle Bedeutung in den Pflanzenlabor-Kursen weiter.
+                        </div>
+                    </div>
+                </div>
+                <div class="col-auto d-none d-md-block">
+                    <div class="card">
+                        <div class="card-body">
+                    <img class="" src="<?php echo $this->router->get_asset_path( "/img/me-galium_400x600.png" ); ?>" alt="Giovina Nicolai" width="200">
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     </div>
     <div class="row mt-3">
