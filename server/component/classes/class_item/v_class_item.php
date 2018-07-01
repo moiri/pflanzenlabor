6738c895421ddb6104eb90b008484d8cddf643ab
Simon Maurer <moirelein@gmail.com>
2018-07-01 15:19:19 +0200

    enable viewport and change layout of classes overview (#7)

--- server/component/classes/class_item/v_class_item.php
+++ server/component/classes/class_item/v_class_item.php
@@ -1,9 +1,9 @@
 <div class="row row-eq-height mt-3">
     <div class="col-12 col-lg-7 pr-lg-1 d-flex">
         <a href="<?php echo $this->router->generate("class", array('id' => $this->id)); ?>" class="list-group-item text-dark invert-link-img w-100">
-            <div class="media">
-                <img class="align-self-center mr-3" src="<?php echo $this->router->get_asset_path("/img/" . $this->img); ?>" alt="Bild zum Kurs <?php echo $this->name; ?>" height="150" width="150">
-                <div class="media-body">
+            <div class="d-flex flex-column flex-sm-row">
+                <div><img class="align-self-center mr-3" src="<?php echo $this->router->get_asset_path("/img/" . $this->img); ?>" alt="Bild zum Kurs <?php echo $this->name; ?>" height="150" width="150"></div>
+                <div>
                     <small><?php echo $this->type; ?> | <?php echo $this->place; ?> | <?php echo $this->time; ?></small>
                     <h4><?php echo $this->name; ?> &ndash;
                         <small><?php echo $this->subtitle; ?></small>
--- server/component/v_header_html.php
+++ server/component/v_header_html.php
@@ -8,7 +8,7 @@
 <meta name="DC.title" content="Pflanzenlabor" />
 <meta name="DC.date" content="2018-01-27" />
 <meta name="DC.language" content="de" />
-<!--<meta name="viewport" content="width=device-width, initial-scale=1" />-->
+<meta name="viewport" content="width=device-width, initial-scale=1" />
 <link rel="stylesheet" type="text/css" href="<?php echo $this->router->get_asset_path("/plugin/bootstrap/css/bootstrap.min.css"); ?>" />
 <link rel="stylesheet" type="text/css" href="<?php echo $this->router->get_asset_path("/css/main.css"); ?>" />
 <script src="<?php echo $this->router->get_asset_path("/plugin/jquery/jquery.min.js"); ?>" type="text/javascript"></script>
