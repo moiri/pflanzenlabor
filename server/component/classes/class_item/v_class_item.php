411e6d573cbec2ba3834b44f6f528737afe680b6
Simon Maurer <moirelein@gmail.com>
2018-07-01 16:07:40 +0200

    invert hover colors once a device gets smaller (assuming that small devices are touch devices)

--- server/component/classes/class_item/v_class_item.php
+++ server/component/classes/class_item/v_class_item.php
@@ -2,7 +2,10 @@
     <div class="col-12 col-lg-7 pr-lg-1 d-flex">
         <a href="<?php echo $this->router->generate("class", array('id' => $this->id)); ?>" class="list-group-item text-dark invert-link-img w-100">
             <div class="d-flex flex-column flex-sm-row">
-                <div><img class="align-self-center mr-3" src="<?php echo $this->router->get_asset_path("/img/" . $this->img); ?>" alt="Bild zum Kurs <?php echo $this->name; ?>" height="150" width="150"></div>
+                <div>
+                    <img class="align-self-center mr-3 d-none d-sm-flex" src="<?php echo $this->router->get_asset_path("/img/" . $this->img); ?>" alt="Bild zum Kurs <?php echo $this->name; ?>" height="150" width="150">
+                    <img class="align-self-center mr-3 d-flex d-sm-none" src="<?php echo $this->router->get_asset_path("/img/hov_" . $this->img); ?>" alt="Bild zum Kurs <?php echo $this->name; ?>" height="150" width="150">
+                </div>
                 <div>
                     <small><?php echo $this->type; ?> | <?php echo $this->place; ?> | <?php echo $this->time; ?></small>
                     <h4><?php echo $this->name; ?> &ndash;
