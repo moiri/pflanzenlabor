d6233a2397c773dde34668026b14f44a0e8a062f
Simon Maurer <moirelein@gmail.com>
2018-08-05 22:55:13 +0200

    sample caption for images

--- server/component/impressions/impression_item/v_img.php
+++ server/component/impressions/impression_item/v_img.php
@@ -1,3 +1,4 @@
-<div class="mb-3 archive" align="center">
+<figure class="figure mb-3 archive" align="center">
     <img class="img-fluid" src="<?php echo $url; ?>" alt="Bild zum Archiv <?php echo $this->title; ?>">
-</div>
+    <figcaption class="figure-caption text-right">A caption for the above image.</figcaption>
+</figure>
