0a763b697941fe2be54e96c991a725ef3e114500
Simon Maurer <moirelein@gmail.com>
2018-09-02 14:39:57 +0200

    improve responsiveness of me

--- server/component/me/v_me.php
+++ server/component/me/v_me.php
@@ -1,38 +1,24 @@
-<div class="container-fluid">
+<div class="container">
     <div class="row">
         <?php $this->print_nav(); ?>
     </div>
-    <div class="row">
-        <div class="container">
-            <div class="card">
-                <div class="card-body">
-                    <h1>Giovina Nicolai</h1>
-                </div>
-            </div>
-        </div>
+    <div class="card card-body">
+        <h1>Giovina Nicolai</h1>
     </div>
-    <div class="row mt-3">
-        <div class="container">
-            <div class="row row-eq-height">
-                <div class="col pr-md-0 d-flex mb-3">
-                    <div class="card w-100">
-                        <div class="card-body">
+    <div class="row row-eq-height mt-3">
+        <div class="col mb-3">
+            <div class="card card-body h-100">
 <p>Giovina Nicolai ist Dipl. Drogistin HF, Galenikerin und Bierbrauerin.
 Sie ist in einer Berner Apotheke für die Herstellung von Arzneimitteln verantwortlich und ist Prüfungsexpertin für Galenik an der Höheren Fachschule für Drogistinnen und Drogisten, Neuch&acirc;tel.</p>
 In Bern aufgewachsen und wohnhaft, interessierte sie sich schon früh für Pflanzen und die Naturheilkunde und bildete sich über die Jahre in Pflanzenwissen, Pharmatechnologie und Kosmetikherstellung weiter.
 In einem dreijährigen Aufenthalt in London widmete sie sich einer besonderen Pflanze, dem Hopfen, und erlernte den Beruf der Bierbrauerin.
 Sie braute selbst und gab Kurse im Bierbrauen.
 Heute gibt sie ihr umfassendes Wissen über Pflanzen, deren Vorkommen und Wirkung, aber auch deren kulturelle Bedeutung in den Pflanzenlabor-Kursen weiter.
-                        </div>
-                    </div>
-                </div>
-                <div class="col-auto d-none d-md-block d-flex mb-3">
-                    <div class="card w-100">
-                        <div class="card-body">
-                            <img class="img-fluid" src="<?php echo $this->router->get_asset_path( "/img/me_400x400.png" ); ?>" alt="Giovina Nicolai" width="400px">
-                        </div>
-                    </div>
-                </div>
+            </div>
+        </div>
+        <div class="col-lg-auto mb-3 h-100">
+            <div class="card card-body">
+                <img class="img-fluid mx-auto" src="<?php echo $this->router->get_asset_path( "/img/me_400x400.png" ); ?>" alt="Giovina Nicolai" width="400px">
             </div>
         </div>
     </div>
