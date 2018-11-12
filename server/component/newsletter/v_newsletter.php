4fb2f51ad072043bac43aaa9921795e7cf9a9998
Simon Maurer <moirelein@gmail.com>
2018-11-12 17:55:11 +0100

    add short description

--- server/component/newsletter/v_newsletter.php
+++ server/component/newsletter/v_newsletter.php
@@ -5,9 +5,11 @@
     <div class="card card-body">
         <h1>Newsletter Abonnieren</h1>
     </div>
-<!--    <div class="card card-body mt-3">
-        Ich möchte über Neuigkeiten des Pflanzenlabor informiert werden.
-    </div>-->
+    <div class="card card-body mt-3">
+        <p>Im Pflanzenlabor Newsletter wirst du jeweils über das neuste Kursangebot, sowie über spannende Projekte rund ums Pflanzenlabor informiert.</p>
+        <p>Diese Neuigkeiten werden ca. einmal im Monat per E-Mail versendet.</p>
+        Abmelden kannst du dich jederzeit, indem du auf ein Newsletter-Mail mit STOP antwortest.
+    </div>
     <div class="card mt-3">
         <h5 class="card-header">Newsletter</h5>
         <div class="card-body">
