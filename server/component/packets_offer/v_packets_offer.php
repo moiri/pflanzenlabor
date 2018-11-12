738b05453eb83aa45bfe25ad01859e59496f01a8
Simon Maurer <moirelein@gmail.com>
2018-11-12 18:17:51 +0100

    use alert instread of card

--- server/component/packets_offer/v_packets_offer.php
+++ server/component/packets_offer/v_packets_offer.php
@@ -5,9 +5,9 @@
     <div class="card card-body mb-3">
         <h1>Pflanzenp&auml;ckli Angebote</h1>
     </div>
-    <div class="card card-body mb-3">
+    <div class="alert alert-warning mb-3">
         <span>Das online Portal für den Kauf von Pflanzenp&auml;ckli ist in Bearbeitung und wird bald aufgeschaltet.
-        Als Übergangslösung bitte ich dich P&auml;ckli direkt per Email (info@pflanzenlabor.ch) oder über das <a href="<?php echo $this->router->generate('contact'); ?>">Kontaktformular</a> zu bestellen.</span>
+        Als Übergangslösung bitte ich dich P&auml;ckli direkt per Email (info@pflanzenlabor.ch) oder über das <a class="alert-link" href="<?php echo $this->router->generate('contact'); ?>">Kontaktformular</a> zu bestellen.</span>
     </div>
     <?php $this->print_items(); ?>
     <div class="row">
