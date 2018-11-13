6cb22b237d8db9580487a2c97a9982c28733cca9
Simon Maurer <moirelein@gmail.com>
2018-11-13 14:14:01 +0100

    temporary content update for vauchers

--- server/component/vauchers/v_vauchers.php
+++ server/component/vauchers/v_vauchers.php
@@ -4,7 +4,10 @@
     </div>
     <div class="card card-body mb-3">
         <h1>Gutscheine</h1>
-        <p>Verschenke einen Ausflug mit dem Pflanzenlabor.</p>
+    </div>
+    <div class="alert alert-warning mb-3">
+        <span>Das online Portal für den Kauf von Gutscheinen ist in Bearbeitung und wird bald aufgeschaltet.
+        Als Übergangslösung bitte ich dich Gutscheine direkt per Email (info@pflanzenlabor.ch) oder über das <a class="alert-link" href="<?php echo $this->router->generate('contact'); ?>">Kontaktformular</a> zu bestellen.</span>
     </div>
     <?php $this->print_items(); ?>
     <div class="row">
--- server/component/vauchers/v_vauchers_item.php
+++ server/component/vauchers/v_vauchers_item.php
@@ -4,13 +4,13 @@
     </div>
     <div class="card-body row">
         <div class="col-auto">
-            <img class="" src="<?php echo $this->router->get_asset_path("/img/" . $img); ?>" alt="Bild zum Angebot <?php echo $title; ?>" height="100" width="100">
+            <img class="" src="<?php echo $this->router->get_asset_path("/img/vauchers/" . $img); ?>" alt="Bild zum Angebot <?php echo $title; ?>" height="100" width="100">
         </div>
         <div class="col-12 col-md align-self-start order-md-1 order-last mt-2 mt-md-0">
             <?php echo $text; ?>
         </div>
         <div class="col col-md-auto order-md-2">
-            <a href="<?php echo $this->router->generate("vauchers_enroll", array("id" => $id)); ?>" class="btn btn-primary w-100 mb-3">Kaufen</a>
+            <!--<a href="<?php echo $this->router->generate("vauchers_enroll", array("id" => $id)); ?>" class="btn btn-primary w-100 mb-3">Kaufen</a>-->
             <button type="button" class="btn btn-outline-secondary btn-sm w-100" disabled><?php echo $price; ?></button>
         </div>
     </div>
