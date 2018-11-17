277d747802c158fa7fade7835956ddcfde243f9a
Simon Maurer <moirelein@gmail.com>
2018-11-17 12:49:25 +0100

    fix typos

--- server/component/packets/v_packets.php
+++ server/component/packets/v_packets.php
@@ -10,8 +10,8 @@
             <div class="card card-body h-100">
                 <p>Mit dem Planzenpäckli hast du die Gelegenheit eine Pflanze Schritt für Schritt kennen zu lernen und das mit all deinen Sinnen!</p>
                 <p>Mit einem Jahresabo wird dir all zwei Monate ein Päckli per Post nach Hause geschickt. Darin befinden sich vier Kuverts. Mit jedem Kuvert erfährst du etwas mehr über eine bestimmte Pflanze.</p>
-                <p>Gestartet wird mit dem Erfahren über deine Sinne – Mit einem Tee, ohne dass du weisst was für ein Kraut es ist. Danach hast du die Gelegenheit dich mit dieser Pflanze durch ein Ausmalbild auseinanderzusetzen. Im dritten Kuvert erfährst du, um welche Pflanze es sich handelt und kannst verschiedene Infos darüber lesen. Schlussendlich, im vierten Kuvert, erhälst du vertiefte Infos sowie eine künstlerische Interpretation dieser Pflanze.</p>
-                <p>So kannst du regelmässig eine neue Pflanze kennenlernen – Zuerst Vorurteilslos mit deinen Sinnen, gefolgt von konzentriertem Wissen.</p>
+                <p>Gestartet wird mit dem Erfahren über deine Sinne – mit einem Tee, ohne dass du weisst was für ein Kraut es ist. Danach hast du die Gelegenheit dich mit dieser Pflanze durch ein Ausmalbild auseinanderzusetzen. Im dritten Kuvert erfährst du, um welche Pflanze es sich handelt und kannst verschiedene Infos darüber lesen. Schlussendlich, im vierten Kuvert, erhälst du vertiefte Infos sowie eine künstlerische Interpretation dieser Pflanze.</p>
+                <p>So kannst du regelmässig eine neue Pflanze kennenlernen – zuerst Vorurteilslos mit deinen Sinnen, gefolgt von konzentriertem Wissen.</p>
                 <p>Interessiert? Dann klicke auf untenstehenden Knopf um das aktuelle Angebot anzusehen.</p>
                 <a href="<?php echo $this->router->generate("packets_offer"); ?>" class="btn btn-primary">Zum Angebot</a>
             </div>
