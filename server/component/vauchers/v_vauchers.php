5a0571fb9fbb123d83754cbedd6ab03af76d8221
Simon Maurer <moirelein@gmail.com>
2018-12-24 14:27:52 +0100

    add vaucher description (close #43)

--- server/component/vauchers/v_vauchers.php
+++ server/component/vauchers/v_vauchers.php
@@ -5,6 +5,21 @@
     <div class="card card-body mb-3">
         <h1>Gutscheine</h1>
     </div>
+    <div class="row row-eq-height mt-3">
+        <div class="col mb-3">
+            <div class="card card-body h-100">
+<p>Ein, von Hand geschriebener, Gutschein wird per Post an die Lieferadresse gesendet.
+Falls gewünscht, kannst du noch einen Grusstext hinzufügen, welcher ebnfalls von Hand geschreiben zum Gutschein zugelegt wird.</p>
+<p>Jede Alraune auf der Gutscheinkarte ist ein kleines Unikat. Entweder ist sie ein Linoldruck oder von Hand gezeichnet. Deshalb kann die Gutscheinkarte von der nebenstehenden Foto leicht abweichen.</p>
+Die beschenkte Person kann mit dem Gutschein einen beliebigen Kurs aus der geschenkten Kategorie (Pflanzenexkursion; Pflanzenwerkstatt oder Pflanzenspaziergang) besuchen.
+            </div>
+        </div>
+        <div class="col-lg-auto mb-3 h-100">
+            <div class="card card-body">
+                <img class="img-fluid mx-auto" src="<?php echo $this->router->get_asset_path( "/img/vaucher_400x400.jpg" ); ?>" alt="Giovina Nicolai" width="250px">
+            </div>
+        </div>
+    </div>
     <?php $this->print_items(); ?>
     <div class="row">
         <?php $this->print_footer(); ?>
