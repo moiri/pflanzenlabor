628c16316890e1e19f92869e7181a651ba489ed0
Simon Maurer <moirelein@gmail.com>
2019-04-27 15:35:04 +0200

    center cite text

--- server/component/impressions/impression_item/v_cite.php
+++ server/component/impressions/impression_item/v_cite.php
@@ -1,6 +1,6 @@
 <div class="card card-body border-0">
     <blockquote class="blockquote m-auto">
-        <p class="mb-0">"<?php echo $content; ?>"</p>
+        <p class="mb-0 text-center">"<?php echo $content; ?>"</p>
         <footer class="blockquote-footer"><?php echo $name; ?></footer>
     </blockquote>
 </div>
