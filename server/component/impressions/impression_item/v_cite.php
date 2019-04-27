894171ce4a9a5602cb75be0f41f9f7990b4311eb
Simon Maurer <moirelein@gmail.com>
2019-04-27 15:37:16 +0200

    text-right cite name

--- server/component/impressions/impression_item/v_cite.php
+++ server/component/impressions/impression_item/v_cite.php
@@ -1,6 +1,6 @@
 <div class="card card-body border-0">
     <blockquote class="blockquote m-auto">
         <p class="mb-0 text-center">"<?php echo $content; ?>"</p>
-        <footer class="blockquote-footer"><?php echo $name; ?></footer>
+        <footer class="blockquote-footer text-right"><?php echo $name; ?></footer>
     </blockquote>
 </div>
