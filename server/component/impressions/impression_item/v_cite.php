e49c190e0a27f58d97752079a0f41ffad4dc8576
Simon Maurer <moirelein@gmail.com>
2019-04-27 15:49:29 +0200

    remove qutoation marks

--- server/component/impressions/impression_item/v_cite.php
+++ server/component/impressions/impression_item/v_cite.php
@@ -1,6 +1,6 @@
 <div class="card card-body border-0">
     <blockquote class="blockquote m-auto">
-        <p class="mb-0 text-center">"<?php echo $content; ?>"</p>
+        <p class="mb-0 text-center"><?php echo $content; ?></p>
         <?php $this->print_cite_name($name); ?>
     </blockquote>
 </div>
