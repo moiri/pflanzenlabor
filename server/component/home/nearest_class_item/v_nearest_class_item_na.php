b391f264857c2dcf15a8a349c84a2f1a0f35b139
Simon Maurer <moiri@users.noreply.github.com>
2019-12-10 22:01:50 +0100

    change nearest date to 2020

--- server/component/home/nearest_class_item/v_nearest_class_item_na.php
+++ server/component/home/nearest_class_item/v_nearest_class_item_na.php
@@ -1,3 +1,3 @@
 <div class="mt-auto mb-auto"><p>Momentant ist Winterpause.</p>
-<p>N&auml;chste Kurse finden ab Fr&uuml;hling 2019 statt.</p>
+<p>N&auml;chste Kurse finden ab Fr&uuml;hling 2020 statt.</p>
 </div>
