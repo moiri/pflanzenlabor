1c55c7327ea995b4c25bc0c3a87eb9f533797f03
Simon Maurer <moirelein@gmail.com>
2020-06-06 13:06:15 +0200

    change payment placeholder image

--- server/component/payment/v_delivery_address_alt.php
+++ server/component/payment/v_delivery_address_alt.php
@@ -1,3 +1,3 @@
 <div class="card card-body h-100 d-flex justify-content-center">
-    <img class="mx-auto" src="<?php echo $this->router->get_asset_path("/img/assets/packets/gift_100x100.png"); ?>" alt="Bild zum Angebot Abo zum Verschenken" height="100" width="100">
+    <img class="mx-auto" src="<?php echo $this->router->get_asset_path("/img/assets/packets/gift_250x100.png"); ?>" alt="Bild zum Angebot Abo zum Verschenken" height="100" width="250">
 </div>
