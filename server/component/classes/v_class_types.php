1121983f4c91cf79c82657c64a5c1f8c000b0919
Simon Maurer <moirelein@gmail.com>
2022-02-13 19:42:20 +0100

    address changes of #71

--- server/component/classes/v_class_types.php
+++ server/component/classes/v_class_types.php
@@ -3,9 +3,6 @@
         <button id="filter-all" type="button" class="btn btn-outline-secondary active btn-block">Alle Kurse</button>
     </div>
     <div class="col-md col-12 mb-1 mb-md-0">
-        <button id="filter-Pflanzenexkursion" type="button" class="btn btn-outline-secondary btn-block">Exkursion</button>
-    </div>
-    <div class="col-md col-12 mb-1 mb-md-0">
         <button id="filter-Pflanzenwerkstatt" type="button" class="btn btn-outline-secondary btn-block">Werkstatt</button>
     </div>
     <div class="col-md col-12 mb-1 mb-md-0">
@@ -15,49 +12,11 @@
         <button id="filter-Bierbrau_Kurs" type="button" class="btn btn-outline-secondary btn-block">Bierbrau Kurs</button>
     </div>
 </div>
-<div class="target-Pflanzenexkursion collapse">
-    <div class="row row-eq-height mt-3">
-        <div class="col-12 col-lg-7 pr-lg-1 d-flex">
-            <div class="card card-body bg-light">
-                <p>
-Die Exkursionen finden in der freien Natur statt und widmen sich den Pflanzen in ihrem natürlichen Umfeld. Du lernst die Pflanzen von ihrer botanischen, kulturellen, gesundheitlichen bis hin zu ihrer mystischen Bedeutung kennen.
-                </p>
-                <p>
-Um die Pflanzen in ihrer Ganzheit zu erfassen und um Zeit für Experimente und eigene Erfahrungen zu haben, steht jeweils nur eine Pflanzenart im Zentrum einer Exkursion.
-                </p>
-Am Mittag gibt es ein Essen, welches über dem Feuer zubereitet wird. Jede Exkursion beinhaltet einen kleinen Werkstattteil, in dem die Pflanze verarbeitet wird. Das Resultat kann natürlich mit nach Hause genommen werden.
-            </div>
-        </div>
-        <div class="col mb-1 mb-lg-0 d-lg-flex d-none">
-            <div class="card w-100 card-body bg-light">
-                <dl class="row mb-0">
-                    <dt class="col-sm-3">Ort</dt>
-                    <dd class="col-sm-9">Draussen wo die Pflanze wächst in der Umgebung Bern. Erreichbar mit ÖV.</dd>
-
-                    <dt class="col-sm-3">Dauer</dt>
-                    <dd class="col-sm-9">6 Stunden</dd>
-
-                    <dt class="col-sm-3">Wann</dt>
-                    <dd class="col-sm-9">Durchs ganze Jahr, Samstag oder Sonntag</dd>
-
-                    <dt class="col-sm-3">Preis</dt>
-                    <dd class="col-sm-9 mb-0">CHF 120.00 Inkl. Kursmaterial und Mittagessen</dd>
-                </dl>
-            </div>
-        </div>
-    </div>
-</div>
 <div class="target-Pflanzenwerkstatt collapse">
     <div class="row row-eq-height mt-3">
         <div class="col-12 col-lg-7 pr-lg-1 d-flex">
             <div class="card card-body bg-light">
-                <p>
-Das Bestimmen, Ernten und Verarbeiten der ausgewählten Pflanze steht im Zenturm einer Pflanzenwerkstatt.
-                </p>
-                <p>
-Je nach Ernteertrag stellen wir, wie immer in der freien Natur, verschiedene Köstlichkeiten her. Die entstandenen Produkte werden beim Zvieri degustiert und/oder können mit nach Hause genommen werden.
-                </p>
-Eine Werkstatt wird abgerundet mit spannenden bis skurrilen Details zur bereffenden Pflanze.
+Das Verarbeiten und Erfahren der ausgewählten Pflanze steht im Zenturm einer Pflanzenwerkstatt. Meistens finden diese in der freien Natur statt, dort wo die Pflanze wächst. Eine Werkstatt wird jeweils abgerundet mit spannenden bis skurrilen Details zur betreffenden Pflanze.
             </div>
         </div>
         <div class="col mb-1 mb-lg-0 d-lg-flex d-none">
@@ -67,13 +26,10 @@ Eine Werkstatt wird abgerundet mit spannenden bis skurrilen Details zur bereffen
                     <dd class="col-sm-9">Draussen wo die Pflanze wächst in der Umgebung Bern. Erreichbar mit ÖV.</dd>
 
                     <dt class="col-sm-3">Dauer</dt>
-                    <dd class="col-sm-9">3 Stunden</dd>
+                    <dd class="col-sm-9">3 bis 5 Stunden</dd>
 
                     <dt class="col-sm-3">Wann</dt>
                     <dd class="col-sm-9">Durchs ganze Jahr, Samstag oder Sonntag</dd>
-
-                    <dt class="col-sm-3">Preis</dt>
-                    <dd class="col-sm-9 mb-0">CHF 60.00 Inkl. Kursmaterial und Z'Vieri</dd>
                 </dl>
             </div>
         </div>
@@ -84,9 +40,9 @@ Eine Werkstatt wird abgerundet mit spannenden bis skurrilen Details zur bereffen
         <div class="col-12 col-lg-7 pr-lg-1 d-flex">
             <div class="card card-body bg-light">
                 <p>
-Jede Jahreszeit birgt in der Pflanzenwelt ihre eigene Schönheit und Faszination.
+Jede Jahreszeit birgt in der Pflanzenwelt ihre eigene Schönheit und Faszination. Mit monatlichen Spaziergängen können wir nebst Pflanzenwissen auch die Änderungen der Vegetation beobachten.
                 </p>
-Die abendlichen Pflanzenspaziergänge finden jede zweite Woche statt. Bei diesen Rundgängen betrachten wir die Pflanzen, welche um uns herum wachsen. Zusammen bestimmen wir, uns unbekannte Pflanzen und nutzen diese Gelegenheit, gegenseitig Erfahrungen, Geschichten und Wissen auszutauschen.
+Durch die Leitung von unterschiedlichen Pflanzen-Fachleuten gestalten sich diese Spaziergänge in vielfältiger Weise.
             </div>
         </div>
         <div class="col mb-1 mb-lg-0 d-lg-flex d-none">
@@ -99,10 +55,7 @@ Die abendlichen Pflanzenspaziergänge finden jede zweite Woche statt. Bei diesen
                     <dd class="col-sm-9">2 Stunden</dd>
 
                     <dt class="col-sm-3">Wann</dt>
-                    <dd class="col-sm-9">Jede zweite Woche, Mittwoch oder Donnerstag</dd>
-
-                    <dt class="col-sm-3">Preis</dt>
-                    <dd class="col-sm-9 mb-0">CHF 20.00</dd>
+                    <dd class="col-sm-9">Durchs ganze Jahr, einmal monatlich</dd>
                 </dl>
             </div>
         </div>
@@ -134,9 +87,6 @@ Die Braukurse finden, vor Regen geschützt, draussen statt.
 
                     <dt class="col-sm-3">Wann</dt>
                     <dd class="col-sm-9">Der Brautag findet am Wochenende statt</dd>
-
-                    <dt class="col-sm-3">Preis</dt>
-                    <dd class="col-sm-9 mb-0">CHF 180.00 Inkl. Kursmaterial, Mittagessen und gebrautem Bier</dd>
                 </dl>
             </div>
         </div>
