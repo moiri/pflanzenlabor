1af8d29207c53a84d01f7e32bab40614be92994f
Simon Maurer <moirelein@gmail.com>
2023-05-08 22:33:10 +0200

    update pflanzenpäckli landing page

--- server/component/packets/v_packets.php
+++ server/component/packets/v_packets.php
@@ -8,17 +8,23 @@
     <div class="row row-eq-height mt-3">
         <div class="col mb-3">
             <div class="card card-body h-100">
-                <p>Mit dem Planzenpäckli hast du die Gelegenheit eine Pflanze Schritt für Schritt kennen zu lernen und das mit all deinen Sinnen!</p>
-                <p>Mit einem Jahresabo wird dir all zwei Monate ein Päckli per Post nach Hause geschickt. Darin befinden sich vier Kuverts. Mit jedem Kuvert erfährst du etwas mehr über eine bestimmte Pflanze.</p>
-                <p>Gestartet wird mit dem Erfahren über deine Sinne – mit einem Tee, ohne dass du weisst was für ein Kraut es ist. Danach hast du die Gelegenheit dich mit dieser Pflanze durch ein Ausmalbild auseinanderzusetzen. Im dritten Kuvert erfährst du, um welche Pflanze es sich handelt und kannst verschiedene Infos darüber lesen. Schlussendlich, im vierten Kuvert, erhälst du vertiefte Infos sowie eine künstlerische Interpretation dieser Pflanze.</p>
-                <p>So kannst du regelmässig eine neue Pflanze kennenlernen – zuerst Vorurteilslos mit deinen Sinnen, gefolgt von konzentriertem Wissen.</p>
-                <p>Interessiert? Dann klicke auf untenstehenden Knopf um das aktuelle Angebot anzusehen.</p>
+                <p>Tauche in eine neue Pflanzenwelt ein – Schritt für Schritt, mit all deinen Sinnen.</p>
+                <dl>
+                    <dt>Erfahren</dt>
+                    <dd>Duft und Geschmack eines Teekrauts wirst du in einem ersten Schritt kennenlernen, ohne dass dir der Name der Pflanze bekannt ist. Eine Anleitung zum achtsamen Teegenuss begleitet dich dabei.</dd>
+                    <dt>Befassen</dt>
+                    <dd>Eine ornamentale Illustration in Schwarz-Weiss, zusammen mit einem kurzen Zitat, verrät dir mehr über die Pflanze. In diesem Schritt kannst du dich visuell an die Pflanze annähern.</dd>
+                    <dt>Informieren</dt>
+                    <dd>In einem detaillierten Porträt – samt botanischer Zeichnung – erfährst du Wissenswertes über die Pflanze, ihre Heilwirkung und Anwendung.</dd>
+                    <dt>Vertiefen</dt>
+                    <dd>Zur Abrundung findest du eine künstlerische Interpretation, einen ergänzenden Text, eine Anleitung oder ein Rezept – als Inspiration, diese neue (oder alte) Pflanzenbekanntschaft zu intensivieren.</dd>
+                </dl>
                 <a href="<?php echo $this->router->generate("packets_offer"); ?>" class="btn btn-primary">Zum Angebot</a>
             </div>
         </div>
         <div class="col-lg-auto mb-3">
             <div class="card card-body h-100">
-                <img class="img-fluid mx-auto" src="<?php echo $this->router->get_asset_path( "/img/packets_400x400.png" ); ?>" alt="Pflanzenp&auml;ckli" width="400px">
+                <img class="img-fluid mx-auto" src="<?php echo $this->router->get_asset_path( "/img/packets_400x400.jpg" ); ?>" alt="Pflanzenp&auml;ckli" width="400px">
             </div>
         </div>
     </div>
